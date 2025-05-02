55e0e76c3b4d5c1d9bbfe8cfcc9fcc1d071a9b36
grumpiestcub <grumpiestcub@gmail.com>
2025-05-01 21:37:04 -0500

    removed loading bar thing, didn't like it. fixed up some css now learning tailwinds

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # website v1 // a newsletter and a way to share with friends
+a site to house music, pictures, videos, and text. 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,13 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../css/index.css'
 import Soon from '../components/Soon.jsx'
-import WrapperProgress from '../components/WrapperProgress.jsx'
 import TypeInfo from '../components/TypeInfo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Soon />
-    <WrapperProgress />
     <TypeInfo />
   </StrictMode>,
 )
