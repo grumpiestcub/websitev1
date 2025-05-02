0246017111c72899f508f3838873a40eb8457cb4
grumpiestcub <grumpiestcub@gmail.com>
2025-05-01 20:49:07 -0500

    installed react router, preparing for multiple pages

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './css/index.css'
-import Soon from './components/Soon.jsx'
-import WrapperProgress from './components/WrapperProgress.jsx'
-import TypeInfo from './components/TypeInfo.jsx'
+import '../css/index.css'
+import Soon from '../components/Soon.jsx'
+import WrapperProgress from '../components/WrapperProgress.jsx'
+import TypeInfo from '../components/TypeInfo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
